33de73243865abda8637d0e9b6a96798113a9027
shash-2106 <raoshashwati@gmail.com>
2021-05-20 12:29:14 +0530

    p81

--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -6,11 +6,74 @@ import { TextInput } from 'react-native';
 
 
 export default class SettingScreen extends React.Component{
-   render(){
-       return(
-           <Text>Settings</Text>
-       )
-   }
+    constructor(){
+        super();
+        this.state={
+            firstName:'',
+            lastName:'',
+            email:'',
+            address:'',
+            contact:'',
+            docId:''
+        }
+    }
+    componentDidMount(){
+        this.getUserDetails()
+    }
+    getUserDetails=()=>{
+        var email = firebase.auth().currentUser.email
+        db.collection("users").where("email_id","==",email).get().then((snapshot)=>{snapshot.forEach(doc=>{
+            var data = doc.data()
+           
+            this.setState({
+               
+                email:data.email_id,
+                firstName:data.first_name,
+                lastName:data.last_name,
+                contact:data.contact,
+                address:data.address,
+                docId:doc.id
+            })
+        })})
+    // alert(this.state.email)
+    }
+
+    updateUserDetails=()=>{
+        db.collection("users").doc(this.state.docId).update({
+            "first_name":this.state.firstName,
+            "last_name":this.state.lastName,
+            "address":this.state.address,
+            "contact":this.state.contact
+        })
+        alert("user updated successfully");
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+             <View>
+                 <TextInput value ={this.state.firstName} style={styles.textInput} maxLength={8} placeholder={"First Name"} onChangeText={(text)=>{this.setState({
+                     firstName:text
+                 })
+                 }}></TextInput>
+                  <TextInput value={this.state.lastName} style={styles.textInput} maxLength={8} placeholder={"Last Name"} onChangeText={(text)=>{this.setState({
+                     lastName:text
+                 })
+                 }}></TextInput>
+                  <TextInput value={this.state.contact} style={styles.textInput} maxLength={10} placeholder={"Contact"} keyboardType={'numeric'} onChangeText={(text)=>{this.setState({
+                     contact:text
+                 })
+                 }}></TextInput>
+                  <TextInput value={this.state.address} style={styles.textInput} multiline={true} placeholder={"Address"} onChangeText={(text)=>{this.setState({
+                     address:text
+                 })
+                 }}></TextInput>
+                 <TouchableOpacity style={styles.button} onPress={()=>{this.updateUserDetails()}}>
+                     <Text style={styles.buttonText}>Save</Text>
+                 </TouchableOpacity>
+             </View>
+            </View>
+        )
+    }
 }
 const styles = StyleSheet.create({
     container : 
